d200feb75a50ce6d5dd5c4cabd9c2a96e45e909e
hchiam <hchiam@gmail.com>
2020-01-17 17:09:48 -0500

    this works too (for #1)

--- generators/app/templates/_your_first_commit.sh
+++ generators/app/templates/_your_first_commit.sh
@@ -3,13 +3,8 @@
 curl -u '<%= author %>' https://api.github.com/user/repos -d '{"name":"learning-<%= whatLearningLink %>", "description":"<%= description %>"}'
 git remote add origin 'git@github.com:<%= author %>/learning-<%= whatLearningLink %>.git'
 git add .
-git commit -m "Set up repo"
-
+git commit -m "Set up repo for real"
 git remote set-url origin https://github.com/<%= author %>/learning-<%= whatLearningLink %>.git
-
 git push --set-upstream origin master
-git add .
-git commit -m "Set up repo"
-git push;
 echo; echo -------; echo;
 git status
