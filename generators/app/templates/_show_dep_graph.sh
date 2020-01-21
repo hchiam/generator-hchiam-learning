f471e009f6757ccb26dd748c71bc6c7a856865a9
hchiam <hchiam@gmail.com>
2020-01-21 11:55:33 -0500

    max depth and src folder variables (fix #2)

--- generators/app/templates/_show_dep_graph.sh
+++ generators/app/templates/_show_dep_graph.sh
@@ -1,12 +1,20 @@
+function askForMaxDepth() {
+  read -p "Enter max depth: " maxDepth
+}
+
 function installDotIfMissing() {
   if ! [ -x "$(command -v dot)" ]; then
-    echo 'Error: dot graphviz is not set up or installed.'
+    echo 'Error: dot graphviz is not set up or installed. Attempting install now.'
     command -v eslint
   fi
 }
 
+function getSrcFolder() {
+  read -p "Enter src folder: " srcFolder
+}
+
 function createDependencyGraph() {
-  depcruise --exclude "^node_modules" --output-type dot src | dot -T svg > dependencygraph.svg
+  depcruise --max-depth $maxDepth --exclude "^node_modules" --output-type dot $srcFolder | dot -T svg > dependencygraph.svg
   echo 'Open dependencygraph.svg in a browser/viewer.'
 }
 
@@ -16,6 +24,13 @@ function showDependencyGraph() {
   open -a "Google Chrome" dependencygraph.svg || true
 }
 
+askForMaxDepth
 installDotIfMissing
+if [ ! -d src ]; then
+  echo "src folder missing"
+  getSrcFolder
+else
+  srcFolder="src"
+fi
 createDependencyGraph
 showDependencyGraph
