51e915c91ea92105f75c756337dca9e7a5ed13a1
hchiam <hchiam@gmail.com>
2020-01-22 16:14:39 -0500

    fix tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -11,6 +11,15 @@ describe("generator-hchiam-learning:app", () => {
   });
 
   it("creates files", () => {
-    assert.file(["dummyfile.txt"]);
+    assert.file(["src/index.js"]);
+    assert.file(["src/index.test.js"]);
+    assert.file([".eslintignore"]);
+    assert.file([".eslintrc.js"]);
+    assert.file([".gitignore"]);
+    assert.file([".travis.yml"]);
+    assert.file(["package.json"]);
+    assert.file(["README.md"]);
+    assert.file(["show_dep_graph.sh"]);
+    assert.file(["your_first_commit.sh"]);
   });
 });
