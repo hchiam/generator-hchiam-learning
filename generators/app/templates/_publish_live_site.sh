d02f1b0c4ae24960939bb96af0a7cce323397ec7
hchiam <hchiam@gmail.com>
2020-02-11 12:07:10 -0500

    add surge command + improve publish_live_site.sh

--- generators/app/templates/_publish_live_site.sh
+++ generators/app/templates/_publish_live_site.sh
@@ -1,5 +1,42 @@
-npm install -g surge
-surge
+folderToUse=src
+surgePageToPublishTo=hchiam-test.surge.sh
+
+function installSurgeIfMissing() {
+  if ! [ -x "$(command -v surge)" ]; then
+    npm install -g surge
+  fi
+}
+
+function publishToSurge() {
+  surge $folderToUse $surgePageToPublishTo
+}
+
+function openInChrome() {
+  open -na "Google Chrome" --args -incognito $surgePageToPublishTo
+}
+
+installSurgeIfMissing
+
+read -p "Use default src folder? " useSrcFolder
+
+# keep asking until given a response
+while [ -z "$useSrcFolder" ]; do
+  read -p "Use default src folder? " useSrcFolder
+done
+
+re='(y|Y)(es|up|eah)?'
+if [[ $useSrcFolder =~ $re ]]; then
+  echo "I'll take that as a yes. :)"
+  echo "Publishing to $surgePageToPublishTo"
+  publishToSurge
+  openInChrome
+else
+  echo "I'll take that as a no. :)"
+  read -p "What folder to use? " folderToUse
+  read -p "What surge.sh page to publish to? " surgePageToPublishTo
+  publishToSurge
+  openInChrome
+fi
 
 # This file was first created using the Yeoman generator
 # generator-hchiam-learning:
