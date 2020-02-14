398bea1dff8e4de1c2c50cedbb27ad943837634c
hchiam <hchiam@gmail.com>
2020-02-14 00:19:14 -0500

    eslint that works with react

--- generators/app/templates/_.eslintrc.js
+++ generators/app/templates/_.eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   'extends': [
     'google',
+    'plugin:react/recommended',
   ],
   'globals': {
     'Atomics': 'readonly',
@@ -13,6 +14,11 @@ module.exports = {
   'parserOptions': {
     'ecmaVersion': 2018,
     'sourceType': 'module',
+  },
+  'settings': {
+    'react': {
+      'version': 'detect',
+    },
   },
   'rules': {
   },
